9c2b8a54b5760818e476cba30a71b378e792a1f3
Matt <mdg408@nyu.edu>
2017-02-28 13:38:05 -0500

    Write demo script and README explanation

---
+++ dogs_demo.rb
@@ -0,0 +1,20 @@
+require_relative './ormellow.rb'
+
+DBConnection::reset
+
+class Person < SQLObject
+  has_many :dogs, foreign_key: :owner_id
+  belongs_to :house, foreign_key: :house_id
+  finalize!
+end
+
+class House < SQLObject
+  has_many :people, foreign_key: :owner_id
+  finalize!
+end
+
+class Dog < SQLObject
+  belongs_to :owner, foreign_key: :owner_id, class_name: "Person"
+  has_one_through :house, :owner, :house
+  finalize!
+end
