1fb4f2dd0ab85d479e600bd58674582234defc9e
Matt <mdg408@nyu.edu>
2017-02-28 17:00:32 -0500

    Add code snippets to README

--- dogs_demo.rb
+++ dogs_demo.rb
@@ -5,16 +5,17 @@ DBConnection::reset
 class Person < SQLObject
   has_many :dogs, foreign_key: :owner_id
   belongs_to :house, foreign_key: :house_id
-  finalize!
 end
 
 class House < SQLObject
   has_many :people, foreign_key: :owner_id
-  finalize!
 end
 
 class Dog < SQLObject
   belongs_to :owner, foreign_key: :owner_id, class_name: "Person"
   has_one_through :house, :owner, :house
-  finalize!
 end
+
+Person.finalize!
+House.finalize!
+Dog.finalize!
